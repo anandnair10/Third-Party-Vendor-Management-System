2dc7f08bd16c8ddfe25ef017865c03f8a1297e93
ananadnair10 <anandnair551@gmail.com>
2025-08-16 01:15:09 +0530

    initial commit

---
+++ src/main/java/com/capstone/Third_Party_Vendor_Management_System/ThirdPartyVendorManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.Third_Party_Vendor_Management_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdPartyVendorManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdPartyVendorManagementSystemApplication.class, args);
+	}
+
+}
