b6d32902594242155619358b0b7979404bc14d8c
aswathiek <aswathy3k@gmail.com>
2025-08-17 01:03:07 +0530

    Updated the classes

--- src/main/java/com/capstone/Third_Party_Vendor_Management_System/entities/ComplianceDoc.java
+++ src/main/java/com/capstone/Third_Party_Vendor_Management_System/entities/ComplianceDoc.java
@@ -1,10 +1,22 @@
 package com.capstone.Third_Party_Vendor_Management_System.entities;
 
-import jakarta.persistence.Column;
+import com.capstone.Third_Party_Vendor_Management_System.entities.VerificationStatus;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDate;
 
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "compliance_docs")
 public class ComplianceDoc {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "document_id")
     private Long documentId;
 
@@ -20,10 +32,10 @@ public class ComplianceDoc {
     @Column(name = "expiry_date")
     private LocalDate expiryDate;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "verification_status")
     private VerificationStatus verificationStatus;
 
     @Column(name = "file_path", columnDefinition = "TEXT")
     private String filePath;
-
 }
