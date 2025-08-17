d8fbdd39378abf1e83ecdea7a126e9c2af5a5ec8
anandnair010 <anand.nair@godigit.com>
2025-08-17 22:00:10 +0530

    added vendorService and VendorServiceImpl

--- src/main/java/com/capstone/Third_Party_Vendor_Management_System/service/Impl/VendorServiceImpl.java
+++ src/main/java/com/capstone/Third_Party_Vendor_Management_System/service/Impl/VendorServiceImpl.java
@@ -1,4 +1,32 @@
 package com.capstone.Third_Party_Vendor_Management_System.service.Impl;
 
-public class VendorServiceImpl{
+import com.capstone.Third_Party_Vendor_Management_System.entities.Vendor;
+import com.capstone.Third_Party_Vendor_Management_System.repository.VendorRepository;
+import com.capstone.Third_Party_Vendor_Management_System.service.VendorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class VendorServiceImpl implements VendorService {
+
+    @Autowired
+    private VendorRepository vendorRepository;
+
+    @Override
+    public Vendor registerVendor(Vendor vendor) {
+        return vendorRepository.save(vendor);
+    }
+
+    @Override
+    public Vendor getVendorById(Long vendorId) {
+        return vendorRepository.findById(vendorId)
+                .orElseThrow(() -> new RuntimeException("Vendor not found with id " + vendorId));
+    }
+
+    @Override
+    public List<Vendor> getAllVendors() {
+        return vendorRepository.findAll();
+    }
 }
