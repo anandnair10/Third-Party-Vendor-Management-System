ca79b51407640778661b40d1063e34837b05b39d
aswathiek <aswathy3k@gmail.com>
2025-08-18 11:23:53 +0530

    Employee service updated

--- src/main/java/com/capstone/Third_Party_Vendor_Management_System/service/EmployeeService.java
+++ src/main/java/com/capstone/Third_Party_Vendor_Management_System/service/EmployeeService.java
@@ -1,4 +1,18 @@
 package com.capstone.Third_Party_Vendor_Management_System.service;
+import com.capstone.Third_Party_Vendor_Management_System.entities.Employee;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface EmployeeService {
+
+    List<Employee> getAllEmployees();
+
+    Optional<Employee> getEmployeeById(Long id);
+
+    Employee createEmployee(Employee employee);
+
+    Optional<Employee> updateEmployee(Long id, Employee updatedEmployee);
+
+    boolean deleteEmployee(Long id);
 }
--- src/main/java/com/capstone/Third_Party_Vendor_Management_System/service/Impl/EmployeeServiceImpl.java
+++ src/main/java/com/capstone/Third_Party_Vendor_Management_System/service/Impl/EmployeeServiceImpl.java
@@ -1,4 +1,52 @@
 package com.capstone.Third_Party_Vendor_Management_System.service.Impl;
 
-public class EmployeeServiceImpl{
+import com.capstone.Third_Party_Vendor_Management_System.entities.Employee;
+import com.capstone.Third_Party_Vendor_Management_System.repository.EmployeeRepository;
+import com.capstone.Third_Party_Vendor_Management_System.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Optional<Employee> getEmployeeById(Long id) {
+        return employeeRepository.findById(id);
+    }
+
+    @Override
+    public Employee createEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @Override
+    public Optional<Employee> updateEmployee(Long id, Employee updatedEmployee) {
+        return employeeRepository.findById(id).map(employee -> {
+            employee.setDepartment(updatedEmployee.getDepartment());
+            employee.setUsername(updatedEmployee.getUsername());
+            employee.setPassword(updatedEmployee.getPassword());
+            return employeeRepository.save(employee);
+        });
+    }
+
+    @Override
+    public boolean deleteEmployee(Long id) {
+        if (employeeRepository.existsById(id)) {
+            employeeRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
 }
+
