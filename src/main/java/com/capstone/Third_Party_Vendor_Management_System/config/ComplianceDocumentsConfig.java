c52732152fb5ab92cd132d2f985d7526f37f29ec
anandnair010 <anand.nair@godigit.com>
2025-08-19 10:34:02 +0530

    fixed errors in the code

--- src/main/java/com/capstone/Third_Party_Vendor_Management_System/config/ComplianceDocumentsConfig.java
+++ src/main/java/com/capstone/Third_Party_Vendor_Management_System/config/ComplianceDocumentsConfig.java
@@ -16,10 +16,12 @@ public class ComplianceDocumentsConfig {
                 Arrays.asList("Labour License","GST Certificate","PAN card","Service Agreement"));
     }
 
-    ;
-
-
     public static List<String> getRequiredDocs(VendorType type){
         return requiredDocs.getOrDefault(type, Collections.emptyList());
     }
+
+    // just making some comments to check the git status
+    // delete it once the complete push is done
+    // git status says branch uptodate
+    // trying git push
 }
