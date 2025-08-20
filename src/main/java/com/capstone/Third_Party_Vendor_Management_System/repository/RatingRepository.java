022d549c9ea9d557d8684bd608bf6f5a841c96d8
aswathiek <aswathy3k@gmail.com>
2025-08-21 00:46:42 +0530

    Added getaverage rating by vendor id

--- src/main/java/com/capstone/Third_Party_Vendor_Management_System/repository/RatingRepository.java
+++ src/main/java/com/capstone/Third_Party_Vendor_Management_System/repository/RatingRepository.java
@@ -2,9 +2,15 @@ package com.capstone.Third_Party_Vendor_Management_System.repository;
 
 import com.capstone.Third_Party_Vendor_Management_System.entities.Rating;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 
 public interface RatingRepository extends JpaRepository<Rating, Long> {
     List<Rating> findByVendorId(Long id);
     List<Rating> findByEmployeeId(Long id);
+
+    @Query("SELECT AVG(r.ratingValue) FROM Rating r WHERE r.vendor.id =:vendorid")
+    Double findAverageRatingByVendorId(@Param("vendorid")Long vendorId);
 }
